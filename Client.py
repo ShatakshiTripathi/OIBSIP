0cdcdc54ce5f3ce4c1a146e08796d642b5028d47
Shatakshitripathi <136111587+ShatakshiTripathi@users.noreply.github.com>
2023-11-29 18:22:06 +0530

    Client.py

--- Client.py
+++ Client.py
@@ -3,22 +3,23 @@ import threading
 
 def receive_messages(client_socket):
     while True:
-        try:
-            message = client_socket.recv(1024).decode('utf-8')
-            print(message)
-        except:
-            break
+        # Receive data from the server
+        data = client_socket.recv(1024).decode('utf-8')
+        if not data:
+            break 
 
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect(('127.0.0.1', 8888))
+        print(f"Received from server: {data}")
 
-username = input("Enter your username: ")
-client.send(username.encode('utf-8'))
+def start_client():
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client.connect(('127.0.0.1', 8888))
 
-receive_thread = threading.Thread(target=receive_messages, args=(client,))
-receive_thread.start()
+    receive_thread = threading.Thread(target=receive_messages, args=(client,))
+    receive_thread.start()
 
-while True:
-    message = input()
-    client.send(message.encode('utf-8'))
+    while True:
+        client_msg = input("User 1: ")
+        client.send(client_msg.encode('utf-8'))
 
+if __name__ == "__main__":
+    start_client()
